4e20e71abef66a2770337c39c454717eb11f8542
SamiHasAb <79419474+SamiHasAb@users.noreply.github.com>
2021-05-25 15:55:04 +0200

    Revert "test calc"

--- back-end/src/test/java/org/study/app/AppApplicationTests.java
+++ back-end/src/test/java/org/study/app/AppApplicationTests.java
@@ -1,33 +1,13 @@
 package org.study.app;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class AppApplicationTests {
 
-	Calculator calc = new Calculator();
-
-    @Test
-    void itShouldAddTwoNumbers() {
-    	// given
-		int num1 = 20;
-		int num2 = 30;
-
-		// when
-		int result = calc.add(num1, num2);
-
-		// then
-		int expected = 50;
-		assertThat(result).isEqualTo(expected);
+	@Test
+	void contextLoads() {
 	}
-
-    class Calculator {
-        int add(int a, int b) {
-            return a + b;
-        }
-    }
 
 }
